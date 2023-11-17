cff70f233cbc047cfd887a3a6b2a6515882551a2
camilaesouza <camila-emanuele@hotmail.com>
2023-11-17 19:30:26 -0300

    remove files not used and add login

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use \Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response as HttpResponse;
+
+class AuthController extends Controller
+{
+    public function login(Request $request): JsonResponse
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $token = $request->user()->createToken('auth-token')->plainTextToken;
+
+            return response()->json(['token' => $token, 'message' => __('messages.auth.login_success')], HttpResponse::HTTP_OK);
+        }
+
+        return response()->json(['message' => __('messages.auth.login_invalid')], HttpResponse::HTTP_UNAUTHORIZED);
+    }
+
+    public function logout(Request $request): JsonResponse
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json(['message' => __('messages.auth.logout_success')], HttpResponse::HTTP_OK);
+    }
+}
---
+++ resources/lang/pt-BR/messages.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'auth' => [
+        'login_success' => 'Usurário logado com sucesso.',
+        'login_invalid' => 'Credenciais de login inválidas.',
+        'logout_success' => "Usurário deslogado com sucesso.",
+    ]
+];
