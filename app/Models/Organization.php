5afc7a4b4449cd7b27b0ce8c4a66ac0401dd06a3
camilaesouza <camila-emanuele@hotmail.com>
2024-02-01 20:58:16 -0300

    add basic crud for organization admin

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -4,11 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Laravel\Scout\Searchable;
 
 class Organization extends Model
 {
